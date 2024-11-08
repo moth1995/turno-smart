87d83467826c04ee0f506155e57eca80672fb73b
moth1995 <marcos-flores@live.com.ar>
2024-11-08 20:25:12 -0300

    renombrando clase para historiales medicos

--- turno-smart/Models/HistorialesMedicos.cs
+++ turno-smart/Models/HistorialesMedicos.cs
@@ -1,6 +1,6 @@
 ﻿namespace turno_smart.Models
 {
-    public class Historiales_Medicos
+    public class HistorialesMedicos
     {
         public int Id_Historial { get; set; }
         public int Id_Paciente { get; set; }
