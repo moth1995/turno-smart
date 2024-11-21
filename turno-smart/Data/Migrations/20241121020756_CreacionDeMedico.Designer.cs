4778c99103ac54ffb3d23ed51915904102acc2a6
moth1995 <marcos-flores@live.com.ar>
2024-11-20 23:08:20 -0300

    migracion con ultimos datos en la db

--- turno-smart/Data/Migrations/20241121020756_CreacionDeMedico.Designer.cs
+++ turno-smart/Data/Migrations/20241121020756_CreacionDeMedico.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using turno_smart.Data;
 
@@ -11,9 +12,11 @@ using turno_smart.Data;
 namespace turno_smart.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241121020756_CreacionDeMedico")]
+    partial class CreacionDeMedico
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ turno-smart/Data/Migrations/20241121020756_CreacionDeMedico.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace turno_smart.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreacionDeMedico : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
