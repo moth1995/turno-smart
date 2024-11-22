379856526ba07653f05f218371c17ddd3d8397a7
moth1995 <marcos-flores@live.com.ar>
2024-11-22 19:30:39 -0300

    agregado de creacion de paciente

--- turno-smart/ViewModels/PacienteVM/CreatePacienteVM.cs
+++ turno-smart/ViewModels/PacienteVM/CreatePacienteVM.cs
@@ -2,16 +2,16 @@ namespace turno_smart.ViewModels.PacienteVM
 {
     public class CreatePacienteVM
     {
-        public required string Nombre { get; set; }
-        public required string Apellido { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
         public DateTime FechaNacimiento { get; set; }
         public int DNI { get; set; }
-        public required string Domicilio { get; set; }
-        public required string Ciudad { get; set; }
-        public required string Provincia { get; set; }
+        public string Domicilio { get; set; }
+        public string Ciudad { get; set; }
+        public string Provincia { get; set; }
         public int Cobertura { get; set; }
         public int Telefono { get; set; }
-        public required string Email { get; set; }
+        public string Email { get; set; }
         public short Estado { get; set; }
         public DateTime FechaAlta { get; set; }
     }
